24686274a3daaafe8fc4564fc07d2cc6bf5e6dcd
Mtronics Computers <Mtronics Computers@DESKTOP-50UMF19>
2023-02-17 15:45:19 +0530

    Complete project

---
+++ javaproject/src/javaProgPractice/FactorialNumber.java
@@ -0,0 +1,16 @@
+package javaProgPractice;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num=5;
+int temp=1;
+
+for(int i=num;i>=1;i--) {
+	temp=temp*i;
+}
+System.out.println(temp);
+	}
+
+}
